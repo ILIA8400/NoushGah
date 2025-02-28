5b927e31416a48820459b668ac682a7765aa6f27
ilia <ilia2005ah@gmail.com>
2025-02-28 18:25:50 +0330

    ساخت مدل ها

---
+++ NoushGah.Model/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoushGah.Model
+{
+    public abstract class BaseEntity
+    {
+        public virtual int Id { get; set; }
+        public virtual DateTime CreateDate { get; set; }
+        public virtual DateTime UpdateDate { get; set; }
+        public virtual int CreateUserId { get; set; }
+        public virtual int UpdateUserId { get; set; }
+        public virtual bool IsDeleted { get; set; }
+
+    }
+}
